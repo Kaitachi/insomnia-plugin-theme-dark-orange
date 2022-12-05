9abe26b6505822d45bd88f4355a326aae62adb76
Radamés <r.alfaro8@icloud.com>
2022-12-04 23:01:08 -0400

    Updating color scheme to better suit background colors

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ module.exports.themes = [{
         // object will serve as the default overwrites for all properties you add.
         background: {
             default:    'hsl(215,  15%,  15%)',  // primary background color
-            success:    'hsl(115,  82%,  29%)',  // POST request, 200 OK, parameter names
-            notice:     'hsl(215,  82%,  48%)',  // PATCH request
-            warning:    'hsl(279,  98%,  52%)',  // PUT request
-            danger:     'hsl(  6,  99%,  52%)',  // DELETE request
-            surprise:   'hsl( 24, 100%,  50%)',  // accent (Dashboard link, GET request, SEND button, branch button, add plugin button)
-            info:       'hsl(202,   7%,  68%)'   // OPTIONS AND HEAD request
+            surprise:   'hsl( 25, 100%,  45%)',  // GET request & accents (Dashboard link, SEND button, branch button, add plugin button)
+            success:    'hsl(115,  85%,  30%)',  // POST request, 200 OK, parameter names
+            warning:    'hsl(280, 100%,  70%)',  // PUT request
+            notice:     'hsl(225,  95%,  60%)',  // PATCH request
+            danger:     'hsl( 10, 100%,  55%)',  // DELETE request
+            info:       'hsl(  0,   0%,  70%)'   // OPTIONS AND HEAD request
         },
         foreground: {
             default:    'hsl(  0,   0%, 100%)',    // primary font color
@@ -19,11 +19,18 @@ module.exports.themes = [{
         highlight: {
             default:    'hsl( 24, 100%,  40%)'      // highlight color
         },
+
         // The styles object targets sub-components of the Insomnia application.
         styles: {
             sidebar: {
                 background: {
-                    default:    'hsl( 24, 100%,  25%)' // sidebar background
+                    default:    'hsl( 24, 100%,  35%)', // sidebar background
+                    surprise:   'hsl( 25, 100%,  60%)',  // GET request & accents (Dashboard link, SEND button, branch button, add plugin button)
+                    success:    'hsl(115,  70%,  40%)',  // POST request, 200 OK, parameter names
+                    warning:    'hsl(280, 100%,  40%)',  // PUT request
+                    notice:     'hsl(225,  95%,  50%)',  // PATCH request
+                    danger:     'hsl( 10, 100%,  33%)',  // DELETE request
+                    info:       'hsl(  0,   0%,  50%)'   // OPTIONS AND HEAD request
                 },
                 highlight: {
                     default:    'hsl(225,   6%,  75%)', // selected sidebar item
@@ -47,8 +54,23 @@ module.exports.themes = [{
             }
         },
         rawCss: `
+            /* Response Pane > Timeline Tab > Comments */
             .editor .CodeMirror .cm-curl-comment {
                 color: hsl(202, 7%, 68%);
+            }
+
+            /* gRPC Requests */
+            header .method-grpc {
+                color: hsl(180, 85%, 50%) !important;
+            }
+            
+            .sidebar .method-grpc {
+                color: hsl(180, 85%, 50%) !important;
+            }
+            
+            .fa.fa-exclamation-triangle.notice,
+            button span.warning {
+                color: hsl( 10, 100%,  35%) !important;
             }
         `
     }
